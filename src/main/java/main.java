c4cf9132b12933efae5b92c2c1d33dad1b54b891
Helen Zhao <hzha587@aucklanduni.ac.nz>
2016-07-28 18:40:30 +1200

    Created empty Node and Edge classes. Renamed main and main test to fit Java naming conventions

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,5 @@
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/test/java/models/NodeTest.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by helen on 28/07/2016.
+ */
+public class NodeTest {
+}
