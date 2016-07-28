8c0801fae40c39fcdbc58edde69066ac79adc3b4
Helen Zhao <hzha587@aucklanduni.ac.nz>
2016-07-28 19:25:54 +1200

    Added in simple unit tests for Node and Edge

--- src/test/java/models/EdgeTest.java
+++ src/test/java/models/EdgeTest.java
@@ -1,7 +1,26 @@
 package models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
 /**
  * Created by helen on 28/07/2016.
  */
 public class EdgeTest {
+
+    @Test
+    public void testCreateEdge() {
+        Node startNode = mock(Node.class);
+        Node endNode = mock(Node.class);
+        int weight = 4;
+
+        Edge edge = new Edge(startNode, endNode, weight);
+
+        assertEquals(startNode, edge.getStartNode());
+        assertEquals(endNode, edge.getEndNode());
+        assertEquals(weight, edge.getWeight());
+    }
+
 }
--- src/test/java/models/NodeTest.java
+++ src/test/java/models/NodeTest.java
@@ -1,7 +1,30 @@
 package models;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by helen on 28/07/2016.
  */
 public class NodeTest {
+
+    @Test
+    public void testCreateNode() {
+        String name = "test";
+        int weight = 3;
+
+        Node node = new Node(name, weight);
+
+        assertEquals(name, node.getName());
+        assertEquals(weight, node.getWeight());
+        assertEquals(new ArrayList<Edge>(), node.getIncomingEdges());
+        assertEquals(-1, node.getStartTime());
+        assertEquals(-1, node.getProcessor());
+        assertEquals(false, node.getHasRun());
+    }
+
+
 }
