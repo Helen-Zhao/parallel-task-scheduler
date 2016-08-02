c25e188cbadf9a79429b3f1f12cc09a059090476
Helen Zhao <hzha587@aucklanduni.ac.nz>
2016-08-03 09:38:32 +1200

    Unfinished schedulertest

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -1,5 +1,6 @@
 package scheduler;
 
+import models.Edge;
 import models.Node;
 import org.junit.Test;
 
@@ -16,18 +17,35 @@ public class SchedulerTest {
 
     @Test
     public void testScheduler() {
-        ValidNodeFinderInterface nodeFinderMock = mock(ValidNodeFinder.class);
+        ValidNodeFinderInterface nodeFinderMock = mock(ValidNodeFinderInterface.class);
         ProcessorAllocatorInterface processorAllocatorMock = mock(ProcessorAllocator.class);
         SchedulerInterface scheduler = new Scheduler(nodeFinderMock, processorAllocatorMock);
 
 
         List<Node> nodeList = new ArrayList<Node>();
-        nodeList.add(new Node("a", 1));
-        nodeList.add(new Node("b", 2));
-        nodeList.add(new Node("c", 3));
-        nodeList.add(new Node("d", 4));
+        Node a = new Node("a", 1);
+        Node b = new Node("b", 2);
+        b.addIncomingEdge(new Edge(a, b, 1));
 
-        when(nodeFinderMock.checkDependentNodes(nodeList.get(0))).thenReturn();
+        nodeList.add(a);
+        nodeList.add(b);
+
+        List<Node> expected = new ArrayList<Node>();
+        expected.add(a);
+        when(nodeFinderMock.findRootNodes(nodeList)).thenReturn(expected);
+
+        List<Node> expected2 = new ArrayList<Node>();
+        expected.add(b);
+        when(nodeFinderMock.checkDependentNodes(a)).thenReturn(expected2);
+
+
+
+
+        when(processorAllocatorMock.allocateProcessor(new ArrayList<Node>(), a, new ArrayList<Integer>())).thenReturn(true);
+        when(processorAllocatorMock.allocateProcessor(new ArrayList(expected), b, new ArrayList<Integer>())).thenReturn(true);
+        when(processorAllocatorMock.allocateProcessor(new ArrayList(expected), b, new ArrayList<Integer>())).thenReturn(true);
+        when(processorAllocatorMock.allocateProcessor(new ArrayList<Node>(), a, new ArrayList<Integer>())).thenReturn(false);
+
 
         List<Node> schedule = scheduler.createSchedule(nodeList);
 
