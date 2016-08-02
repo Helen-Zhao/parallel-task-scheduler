f031b7d962cd0ba7b40d478e727963d4ba4c7e7d
Jacky Mai <jacky.jy.mai@gmail.com>
2016-08-02 18:01:30 +1200

    Polished method names and comments

--- src/main/java/scheduler/ValidNodeFinder.java
+++ src/main/java/scheduler/ValidNodeFinder.java
@@ -6,11 +6,12 @@ import java.util.List;
 import models.Edge;
 import models.Node;
 
-public class ValidNodeFinder {
+public class ValidNodeFinder implements ValidNodeFinderInterface{
 	
-	// Find nodes with no dependencies
+	// Find nodes with no dependencies (root) by searching for nodes with no incoming edges
 	public void findRootNodes(List<Node> nodes) {
 		List<Node> rootNodes = new ArrayList<Node>();
+		
 		for(Node n : nodes){
 			if(n.getIncomingEdges() == null || n.getIncomingEdges().size() == 0){
 				rootNodes.add(n);
@@ -18,13 +19,15 @@ public class ValidNodeFinder {
 		}
 	}
 	
-	// Check if dependent nodes now have dependencies satisfied
+	// Check whether children or dependent nodes for particular node is satisfied
+	// If yes, add it to a list of satisfied nodes and return it
 	public List<Node> checkDependentNodes(Node n){
 		List<Node> satisfiedNodes = new ArrayList<Node>();
 		List<Edge> edges = n.getOutgoingEdges();
+		
 		for(Edge e: edges){
 			Node endNode = e.getEndNode();
-			if(checkDependencies(endNode)){
+			if(isAvailable(endNode)){
 				satisfiedNodes.add(endNode);
 			}
 		}
@@ -32,9 +35,10 @@ public class ValidNodeFinder {
 		return satisfiedNodes;
 	}
 	
-	// Check Incoming Edges for particular node
-	public boolean checkDependencies(Node n){
+	// Check incoming edges for particular node and determine whether it is available or not 
+	public boolean isAvailable(Node n){
 		List<Edge> edges = n.getIncomingEdges();
+		
 		for(Edge e: edges){
 			Node startNode = e.getStartNode();
 			if(startNode.getHasRun() == false){
--- src/main/java/scheduler/ValidNodeFinderInterface.java
+++ src/main/java/scheduler/ValidNodeFinderInterface.java
@@ -6,6 +6,7 @@ import models.Node;
 public interface ValidNodeFinderInterface {
 
 	public void findRootNodes(List<Node> nodes);
+	
 	public List<Node> checkDependentNodes(Node n);
 	
 }
