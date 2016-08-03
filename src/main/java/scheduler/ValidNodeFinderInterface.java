169da6731d3b80cd2481818f50f06528eac7850e
Benjamin Collins <Bcol602@aucklanduni.ac.nz>
2016-08-03 13:20:28 +1200

    Add New Satisfied Node method taking List of Nodes as input
    Adjustment of method name in interface, new test case added

--- src/main/java/scheduler/ValidNodeFinderInterface.java
+++ src/main/java/scheduler/ValidNodeFinderInterface.java
@@ -7,6 +7,8 @@ public interface ValidNodeFinderInterface {
 
 	public List<Node> findRootNodes(List<Node> nodes);
 	
-	public List<Node> findSatisfiedNodes(Node node);
+	public List<Node> findSatisfiedChildren(Node node);
+	
+	public List<Node> findSatisfiedNodes(List<Node> nodes);
 	
 }
