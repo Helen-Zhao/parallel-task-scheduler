4e94aa0966e2fda94adc364268853619b7809857
Helen Zhao <helen4251996@yahoo.co.nz>
2016-08-08 14:30:42 +1200

    Tentative main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,34 @@
+import inputoutput.InputReader;
+import models.Edge;
+import models.Node;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author helen zhao
+ * Created 08/08/16
+ */
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+			Input params take the form of: input.dot, p, algorithm
+			where input.dot is the input file, p is the number of processors and algorithm specifes
+			what algorithm the scheduler should use
+		 */
+
+
+		String inputFileName = args[0];
+		int numProcessors = Integer.parseInt(args[1]);
+		String algorithm = args[2];
+
+		InputReader inputReader = new InputReader(new File(inputFileName));
+		List<Node> nodeList = inputReader.nodeList;
+		List<Edge> edgeList = inputReader.edgeList;
+
+		SchedulerInterface scheduler = new
 
 	}
 
