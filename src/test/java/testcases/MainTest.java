19b2ac0e03e76590df423ad2105e788f68a8f3f1
Helen Zhao <helen4251996@yahoo.co.nz>
2016-08-09 15:53:58 +1200

    Added in directory finding for all test cases in end-end tests

--- src/test/java/testcases/MainTest.java
+++ src/test/java/testcases/MainTest.java
@@ -1,23 +1,32 @@
 package testcases;
 
-import main.Main;
-import org.junit.Test;
 import junitx.framework.FileAssert;
+import main.Main;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.File;
 
 public class MainTest {
 
+    String dir;
+
+    @Before
+    public void setUp() {
+        String workingDir = System.getProperty("user.dir");
+        String dir = workingDir.substring(0, workingDir.indexOf("/src"));
+    }
+
     @Test
     public void oneProcSimpleTest() {
 
         // Expected output file
-        File expected = new File("./src/main/resources/dotfiles/outputfiles/1_processor_simple-output.dot");
+        File expected = new File(dir + "/src/main/resources/dotfiles/outputfiles/1_processor_simple-output.dot");
 
         // TODO
         /* Varying methods for obtaining the final output file which is to be compared with the expected output file. (Our implementation)
 		  
-		File input = new main.Main.readInput("./src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
+		File input = new main.Main.readInput(dir + "/src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
 		
 		File output = main.Main.writeOutput();
 		*/
@@ -29,12 +38,12 @@ public class MainTest {
     public void twoProcSimpleTest() {
 
         // Expected output file
-        File expected = new File("./src/main/resources/dotfiles/outputfiles/2_processor_simple-output.dot");
+        File expected = new File(dir + "/src/main/resources/dotfiles/outputfiles/2_processor_simple-output.dot");
 
         // TODO
 		/* Varying methods for obtaining the final output file which is to be compared with the expected output file. (Our implementation)
 		  
-		File input = new main.Main.readInput("./src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
+		File input = new main.Main.readInput(dir + "/src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
 		
 		File output = main.Main.writeOutput();
 		*/
@@ -46,12 +55,12 @@ public class MainTest {
     public void fourProcOneSrcOneDestTest() {
 
         // Expected output file
-        File expected = new File("./src/main/resources/dotfiles/outputfiles/4_processor_1_src_1_dest-output.dot");
+        File expected = new File(dir + "/src/main/resources/dotfiles/outputfiles/4_processor_1_src_1_dest-output.dot");
 
         // TODO
 		/* Varying methods for obtaining the final output file which is to be compared with the expected output file. (Our implementation)
 		  
-		File input = new main.Main.readInput("./src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
+		File input = new main.Main.readInput(dir + "/src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
 		
 		File output = main.Main.writeOutput();
 		*/
@@ -61,8 +70,6 @@ public class MainTest {
 
     @Test
     public void fourProcOneSrcThreeDestTest() {
-        String workingDir = System.getProperty("user.dir");
-        String dir = workingDir.substring(0, workingDir.indexOf("/src"));
 
         // Expected output file
         File expected = new File(dir + "/src/main/resources/dotfiles/outputfiles/4_processor_1_src_3_dest-output.dot");
@@ -76,11 +83,9 @@ public class MainTest {
 
     @Test
     public void fourProcThreeSrcOneDestTest() {
-        String workingDir = System.getProperty("user.dir");
-        String dir = workingDir.substring(0, workingDir.indexOf("/src"));
 
         // Expected output file
-        File expected = new File("./src/main/resources/dotfiles/outputfiles/4_processor_3_src_1_dest-output.dot");
+        File expected = new File("/src/main/resources/dotfiles/outputfiles/4_processor_3_src_1_dest-output.dot");
         String[] args = {dir + "/src/main/resources/dotfiles/inputfiles/4_processor_3_src_1_dest.dot", "4", "-o", "4_processor_1_src_1_dest-output"};
         Main.main(args);
 
@@ -92,13 +97,13 @@ public class MainTest {
     public void fourProcThreeSrcTwoDestTest() {
 
         // Expected output file
-        Main main = new Main();
-        File expected = new File("./src/main/resources/dotfiles/outputfiles/4_processor_3_src_2_dest-output.dot");
+
+        File expected = new File(dir + "/src/main/resources/dotfiles/outputfiles/4_processor_3_src_2_dest-output.dot");
 
         // TODO
 		/* Varying methods for obtaining the final output file which is to be compared with the expected output file. (Our implementation)
 		  
-		File input = new main.Main.readInput("./src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
+		File input = new main.Main.readInput(dir + "/src/main/resources/dot_Files/Input_Files/1_processor_simple.dot");
 		
 		File output = main.Main.writeOutput();
 		*/
