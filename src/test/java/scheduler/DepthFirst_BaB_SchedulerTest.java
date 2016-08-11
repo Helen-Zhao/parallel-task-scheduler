09420c8522e824f2846c53ffa65c4bb4dc940aac
Jay Gradon <jaygradon@outlook.com>
2016-08-11 21:29:05 +1200

    Updated CustomMocks and removed commented implementation of ProcessorAllocator

--- src/test/java/scheduler/DepthFirst_BaB_SchedulerTest.java
+++ src/test/java/scheduler/DepthFirst_BaB_SchedulerTest.java
@@ -14,7 +14,7 @@ public class DepthFirst_BaB_SchedulerTest {
 	
 	@Test
 	public void simpleLectureTest() {
-		SchedulerInterface sched = new Mem_DepthFirst_BaB_Scheduler(new ValidNodeFinderCustomMock(), new processMock());
+		SchedulerInterface sched = new Mem_DepthFirst_BaB_Scheduler(new ValidNodeFinderCustomMock(), new ProcessorAllocatorCustomMock());
 		
 		List<Node> nodes = new ArrayList<Node>();
 		Node a = new Node("a", 2);
