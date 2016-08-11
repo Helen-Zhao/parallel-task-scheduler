abb8d9c8e7eb264efa7bed8fc44221fc819e3008
Helen Zhao <helen4251996@yahoo.co.nz>
2016-08-11 12:39:27 +1200

    Removed printed nodes and changed output writer to print all edges at the end of the file. Removed system.out.println

--- src/main/java/inputoutput/OutputWriter.java
+++ src/main/java/inputoutput/OutputWriter.java
@@ -6,7 +6,7 @@ import models.Node;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,9 +14,7 @@ import java.util.List;
  * @author William Lin
  */
 public class OutputWriter {
-	
-	private HashMap<String, Node> printedNodes = new HashMap<String, Node>();
-	
+
 	//main.Main output writer function
 	public OutputWriter(List<Node> scheduleNodes, List<Edge> scheduleEdges, String outputFileName) {
 		
@@ -26,7 +24,6 @@ public class OutputWriter {
 			String dir = ".";
 			if (workingDir.length() > 0 && workingDir.contains("/src")) {
 				dir = workingDir.substring(0, workingDir.indexOf("/src"));
-				System.out.println(dir);
 			}
 
 			//Instantiate PrintWriter object to create and write to file. Set encoding to UTF-8
@@ -37,18 +34,10 @@ public class OutputWriter {
 			for(int i = 0; i < scheduleNodes.size(); i++){
 				Node node = scheduleNodes.get(i);
 				writer.println("\t\t" + node.getName() + "\t\t [Weight=" + node.getWeight() + ", Start=" + node.getStartTime() + ", Processor=" + node.getProcessor() + "];");
-				printedNodes.put(node.getName(), node);
-				for(int j = 0; j < scheduleEdges.size(); j++) {
-					Edge edge = scheduleEdges.get(j);
-					//if start and end nodes of an edge is printed, then print the edge
-					if (printedNodes.containsKey(edge.getStartNode().getName())&&printedNodes.containsKey(edge.getEndNode().getName())) {
-						writer.println("\t\t" + edge.getStartNode().getName() + " -> " + edge.getEndNode().getName() + "\t [Weight=" + edge.getWeight() + "];");
-						if (i != scheduleNodes.size() - 1) {
-							scheduleEdges.remove(edge);
-							j--;
-						}
-					}
-				}
+			}
+
+			for (Edge edge : scheduleEdges) {
+				writer.println("\t\t" + edge.getStartNode().getName() + " -> " + edge.getEndNode().getName() + "\t [Weight=" + edge.getWeight() + "];");
 			}
 			
 			writer.print("}");
@@ -56,10 +45,8 @@ public class OutputWriter {
 			writer.close();
 
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
