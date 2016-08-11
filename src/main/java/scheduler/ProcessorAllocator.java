74ea55fbd85eca8b145d89356156e3075be06ca8
Benjamin Collins <Bcol602@aucklanduni.ac.nz>
2016-08-11 15:41:50 +1200

    Processor allocator modifications
    Debugging use

--- src/main/java/scheduler/ProcessorAllocator.java
+++ src/main/java/scheduler/ProcessorAllocator.java
@@ -48,6 +48,54 @@ public class ProcessorAllocator implements ProcessorAllocatorInterface {
 		return true;
 	}
 
+	/*public int findEarliestStartTime(List<Node> schedule, Node node, int processor) {
+		List<Edge> dependencies = node.getIncomingEdges();
+		int earliestValidStart = 0;
+		int earliestValidEnd = node.getWeight();
+		
+		int foundStartTime = 0;
+		int foundEndTime = 0;
+		
+		// Assumed all dependencies are satisfied
+		// Find earliest start based on dependencies
+		for (Edge edge : dependencies) {
+			if (edge.getStartNode().getProcessor() == processor) {
+				// Start new node after dependency
+				earliestValidStart = edge.getStartNode().getStartTime() + edge.getStartNode().getWeight();
+				earliestValidEnd = earliestValidStart + node.getWeight();
+			} else {
+				// Start new node after dependency and comms time
+				earliestValidStart = edge.getStartNode().getStartTime() + edge.getStartNode().getWeight() + edge.getWeight();
+				earliestValidEnd = earliestValidStart + node.getWeight();
+			}
+		}
+		
+		// Find earliest start based on allocated nodes
+		for (Node snode : schedule) {
+			// If scheduled node is in another processor we don't care when it starts
+			if (snode.getProcessor() == processor) {
+				foundStartTime = snode.getStartTime();
+				foundEndTime = foundStartTime + snode.getWeight();
+				// Current earliest valid start time overlaps another task
+				if (earliestValidStart > foundStartTime && earliestValidStart < foundEndTime) {
+					// Start new node after found node
+					earliestValidStart = foundEndTime;
+					earliestValidEnd = earliestValidStart + node.getWeight();
+				// Current earliest valid end time overlaps another task
+				} else if (earliestValidEnd > foundStartTime && earliestValidEnd < foundEndTime) {
+					earliestValidStart = foundEndTime;
+					earliestValidEnd = earliestValidStart + node.getWeight();
+				// Current allocated times are enveloping another task
+				} else if (foundStartTime > earliestValidStart && foundStartTime < earliestValidEnd) {
+					earliestValidStart = foundEndTime;
+					earliestValidEnd = earliestValidStart + node.getWeight();
+				}
+			}
+		}
+		
+		return earliestValidStart;
+	}*/
+	
 	public int findEarliestStartTime(List<Node> schedule, Node node, int processor) {
 		List<Edge> edges = node.getIncomingEdges();
 		int shortestEndTime = 0;
@@ -55,9 +103,10 @@ public class ProcessorAllocator implements ProcessorAllocatorInterface {
 		
 		// Comparison with already scheduled items for finding minimum starting time
 		for (Node scheduledNode : schedule) {
-			
+			boolean hasEdges = false;
 			// Loops through all the dependency nodes
 			for (Edge edge : edges) {
+				hasEdges = true;
 				Node dependentNode = edge.getStartNode();
 				dependentNodeProc = dependentNode.getProcessor();
 				
