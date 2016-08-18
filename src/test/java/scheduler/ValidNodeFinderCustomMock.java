8afc244f3a716559cca4b7cb581b476195b58f45
Helen Zhao <helen4251996@yahoo.co.nz>
2016-08-18 13:31:16 +1200

    Commented and auto formatted

--- src/test/java/scheduler/ValidNodeFinderCustomMock.java
+++ src/test/java/scheduler/ValidNodeFinderCustomMock.java
@@ -1,49 +1,51 @@
 package scheduler;
+
+import models.Node;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import models.Node;
-import scheduler.ValidNodeFinderInterface;
+/**
+ * @author Jay Gradon
+ */
 
-	public class ValidNodeFinderCustomMock implements ValidNodeFinderInterface {
-		
-		@Override
-		public List<Node> findRootNodes(List<Node> nodes) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+public class ValidNodeFinderCustomMock implements ValidNodeFinderInterface {
 
-		@Override
-		public List<Node> findSatisfiedChildren(Node node) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+    @Override
+    public List<Node> findRootNodes(List<Node> nodes) {
+        return null;
+    }
 
-		@Override
-		public List<Node> findSatisfiedNodes(List<Node> nodes) {
-			List<Node> snodes = new ArrayList<Node>();
-			for(Node node : nodes) {
-				if (node.getName() == "a" && !(node.getHasRun())) {
-					snodes.add(node);
-					return snodes;
-				}
-			}
-			for(Node node : nodes) {
-				if (node.getName() == "b" && !(node.getHasRun())) {
-					snodes.add(node);
-				}
-				if (node.getName() == "c" && !(node.getHasRun())) {
-					snodes.add(node);
-				}
-			}
-			if (snodes.size() == 0) {
-				for(Node node : nodes) {
-					if (node.getName() == "d" && !(node.getHasRun())) {
-						snodes.add(node);
-						return snodes;
-					}
-				}
-			}
-			return snodes;
-		}
-	}
+    @Override
+    public List<Node> findSatisfiedChildren(Node node) {
+        return null;
+    }
+
+    @Override
+    public List<Node> findSatisfiedNodes(List<Node> nodes) {
+        List<Node> snodes = new ArrayList<Node>();
+        for (Node node : nodes) {
+            if (node.getName() == "a" && !(node.getHasRun())) {
+                snodes.add(node);
+                return snodes;
+            }
+        }
+        for (Node node : nodes) {
+            if (node.getName() == "b" && !(node.getHasRun())) {
+                snodes.add(node);
+            }
+            if (node.getName() == "c" && !(node.getHasRun())) {
+                snodes.add(node);
+            }
+        }
+        if (snodes.size() == 0) {
+            for (Node node : nodes) {
+                if (node.getName() == "d" && !(node.getHasRun())) {
+                    snodes.add(node);
+                    return snodes;
+                }
+            }
+        }
+        return snodes;
+    }
+}
