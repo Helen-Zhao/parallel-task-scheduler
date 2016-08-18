073e9e943e24b0a0a6c269b9f7e094509c1935fc
Helen Zhao <helen4251996@yahoo.co.nz>
2016-08-18 12:15:50 +1200

    Auto-formatted and added comments to InputReader and OutputWriter

--- src/main/java/inputoutput/InputReader.java
+++ src/main/java/inputoutput/InputReader.java
@@ -14,146 +14,152 @@ import java.util.regex.Pattern;
 
 /**
  * @author William Lin
- *
+ *         <p>
+ *         InputReader parses a .dot file into a list of nodes and a list of edges
  */
+
 public class InputReader {
-	
-	// List of nodes and edges compiled from input .dot file. Can be accessible from other classes and methods.
-	private List<Node> nodeList = new ArrayList<Node>();
-	private List<Edge> edgeList = new ArrayList<Edge>();
-	
-	public InputReader()  {
 
-	}
+    // List of nodes and edges compiled from input .dot file. Can be accessible from other classes and methods.
+    private List<Node> nodeList = new ArrayList<>();
+    private List<Edge> edgeList = new ArrayList<>();
 
-	// main.Main input reader function
-	public void readFile(File f) throws IOException{
-		BufferedReader br = null;
+    public InputReader() {
 
-		try {
-			String sCurrentLine;
-			br = new BufferedReader(new FileReader(f));
-			// skips the first line as it has text we don't want to extract
-			br.readLine();
+    }
 
-			String patternString = "\\[Weight=\\d+\\];";
-			Pattern pattern = Pattern.compile(patternString);
+    // main.Main input reader function
+    public void readFile(File file) throws IOException {
 
-			while ((sCurrentLine = br.readLine()) != null) {
-				if (!(sCurrentLine.contains("}"))) {
+        BufferedReader br = null;
 
-					//If the line contains "[Weight=x];"
-					if (pattern.matcher(sCurrentLine).find()) {
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(file));
+            // skips the first line as it has text we don't want to extract
+            br.readLine();
 
-						// If the current line does not contain an arrow then it is a node, else it is an edge.
-						if (sCurrentLine.indexOf("->") == -1) {
-							int weight = weightCreator(sCurrentLine);
-							String[] nodeNameParts = sCurrentLine.trim().split("\\s+");
-							String nodeName = nodeNameParts[0];
+            String patternString = "\\[Weight=\\d+\\];";
+            Pattern pattern = Pattern.compile(patternString);
 
-							nodeCreator(nodeName, weight);
-						} else {
-							String[] parts = sCurrentLine.trim().split("->");
-							String firstNodeName = parts[0].trim();
+            while ((sCurrentLine = br.readLine()) != null) {
+                if (!(sCurrentLine.contains("}"))) {
 
-							String[] parts2 = parts[1].trim().split("\\s+");
-							String secondNodeName = parts2[0].trim();
+                    //If the line contains "[Weight=x];"
+                    if (pattern.matcher(sCurrentLine).find()) {
 
-							int weight = weightCreator(sCurrentLine);
+                        // If the current line does not contain an arrow then it is a node, else it is an edge.
+                        if (sCurrentLine.indexOf("->") == -1) {
+                            int weight = weightCreator(sCurrentLine);
+                            String[] nodeNameParts = sCurrentLine.trim().split("\\s+");
+                            String nodeName = nodeNameParts[0];
 
-							edgeCreator(firstNodeName, secondNodeName, weight);
-						}
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw e;
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-			} catch(IOException e2) {
-				e2.printStackTrace();
-				throw e2;
+                            nodeCreator(nodeName, weight);
+                        } else {
+                            String[] parts = sCurrentLine.trim().split("->");
+                            String firstNodeName = parts[0].trim();
 
-			}
-		}
-	}
-	
-	//Getter for list of edges
-	public List<Edge> getEdgeList() {
-		return this.edgeList;
-	}
-	public List<Node> getNodeList() { return this.nodeList; }
-	
-	// Private method used to parse obtained "weight" property into an Integer Type from a String
-	private static int weightCreator(String s) {
-		String[] weightSplit = s.split("=");
-		String weight = weightSplit[1];
-		
-		Pattern p = Pattern.compile("-?\\d+");
-		Matcher m = p.matcher(weight);
-		while(m.find()){
-			return (Integer.parseInt(m.group()));
-		}
-		// If weight is of invalid type or invalid input, return -1 to denote it as invalid.
-		return -1;
-	}
-	
-	// Checks if a node already has that name in the current nodeList
-	// Create a new node if the name is available
-	private void nodeCreator(String nodeName, int weight) {
-		if (!containsNode(nodeName)){
-			Node node = new Node(nodeName, weight);
-			nodeList.add(node);
-		} else {
-			throw new IllegalArgumentException("Node name already exists");
-		}
-	}
-	
-	private void edgeCreator(String firstNodeName, String secondNodeName, int weight) {
-		// Index of first and second node for verification. Also acts as a boolean.
-		int firstNodeIndex = -1;
-		int secondNodeIndex = -1;
-		
-		// If weight is valid, then find the index of both nodes in the list.
-		if (weight != -1){
-			for (int i = 0; i < nodeList.size(); i++){
-				if (firstNodeName.equals(nodeList.get(i).getName())) {
-					firstNodeIndex = i;
-				} else if (secondNodeName.equals(nodeList.get(i).getName())) {
-					secondNodeIndex = i;
-				}
-			}
-			
-			// If both indexes found, then create an edge object using the name of the nodes and it's weight.
-			if ((firstNodeIndex != -1) && (secondNodeIndex != -1)) {
-				Node firstNode = nodeList.get(firstNodeIndex);
-				Node secondNode = nodeList.get(secondNodeIndex);
-				Edge edge = new Edge(firstNode, secondNode, weight);
-				
-				edgeList.add(edge);
-				firstNode.addOutgoingEdge(edge);
-				secondNode.addIncomingEdge(edge);
-			} else {
-				// Indexes not found. Node has not been created yet. File contains an error in the order of node creation/listing
-				throw new IllegalArgumentException("Invalid nodes. Nodes not initialised yet");
-			}
-		} else {
-			// Invalid weight property.
-			throw new IllegalArgumentException("Invalid weight");
-		}
-	}
-	
-	private boolean containsNode(String nodeName) {
-		for(Node node : nodeList) {
-			if(node.getName().equals(nodeName)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
+                            String[] parts2 = parts[1].trim().split("\\s+");
+                            String secondNodeName = parts2[0].trim();
+
+                            int weight = weightCreator(sCurrentLine);
+
+                            edgeCreator(firstNodeName, secondNodeName, weight);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e2) {
+                e2.printStackTrace();
+                throw e2;
+
+            }
+        }
+    }
+
+    //Getter for list of edges
+    public List<Edge> getEdgeList() {
+        return this.edgeList;
+    }
+
+    public List<Node> getNodeList() {
+        return this.nodeList;
+    }
+
+    // Private method used to parse obtained "weight" property into an Integer Type from a String
+    private static int weightCreator(String s) {
+        String[] weightSplit = s.split("=");
+        String weight = weightSplit[1];
+
+        Pattern p = Pattern.compile("-?\\d+");
+        Matcher m = p.matcher(weight);
+        while (m.find()) {
+            return (Integer.parseInt(m.group()));
+        }
+        // If weight is of invalid type or invalid input, return -1 to denote it as invalid.
+        return -1;
+    }
+
+    // Checks if a node already has that name in the current nodeList
+    // Create a new node if the name is available
+    private void nodeCreator(String nodeName, int weight) {
+        if (!containsNode(nodeName)) {
+            Node node = new Node(nodeName, weight);
+            nodeList.add(node);
+        } else {
+            throw new IllegalArgumentException("Node name already exists");
+        }
+    }
+
+    private void edgeCreator(String firstNodeName, String secondNodeName, int weight) {
+        // Index of first and second node for verification. Also acts as a boolean.
+        int firstNodeIndex = -1;
+        int secondNodeIndex = -1;
+
+        // If weight is valid, then find the index of both nodes in the list.
+        if (weight != -1) {
+            for (int i = 0; i < nodeList.size(); i++) {
+                if (firstNodeName.equals(nodeList.get(i).getName())) {
+                    firstNodeIndex = i;
+                } else if (secondNodeName.equals(nodeList.get(i).getName())) {
+                    secondNodeIndex = i;
+                }
+            }
+
+            // If both indexes found, then create an edge object using the name of the nodes and it's weight.
+            if ((firstNodeIndex != -1) && (secondNodeIndex != -1)) {
+                Node firstNode = nodeList.get(firstNodeIndex);
+                Node secondNode = nodeList.get(secondNodeIndex);
+                Edge edge = new Edge(firstNode, secondNode, weight);
+
+                edgeList.add(edge);
+                firstNode.addOutgoingEdge(edge);
+                secondNode.addIncomingEdge(edge);
+            } else {
+                // Indexes not found. Node has not been created yet. File contains an error in the order of node creation/listing
+                throw new IllegalArgumentException("Invalid nodes. Nodes not initialised yet");
+            }
+        } else {
+            // Invalid weight property.
+            throw new IllegalArgumentException("Invalid weight");
+        }
+    }
+
+    private boolean containsNode(String nodeName) {
+        for (Node node : nodeList) {
+            if (node.getName().equals(nodeName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/main/java/inputoutput/OutputWriter.java
+++ src/main/java/inputoutput/OutputWriter.java
@@ -10,43 +10,44 @@ import java.util.List;
 
 /**
  * @author William Lin
+ *         <p>
+ *         OutputWriter takes a file name/path to print to, a list of nodes and a list of edges in the optimal schedule
+ *         and creates a .dot file with the specified name
  */
 public class OutputWriter {
 
-	//main.Main output writer function
-	public OutputWriter() {
+    //main.Main output writer function
+    public OutputWriter() {
 
-	}
+    }
 
-	public void writeFile(List<Node> scheduleNodes, List<Edge> scheduleEdges, String outputFileName) {
+    public void writeFile(List<Node> scheduleNodes, List<Edge> scheduleEdges, String outputFileName) {
+        try {
+            //Get working directory
+            String dir = System.getProperty("user.dir");
 
-		try {
-			//Get working directory
-			String dir = System.getProperty("user.dir");
+            //Instantiate PrintWriter object to create and write to file. Set encoding to UTF-8
+            PrintWriter writer = new PrintWriter(dir + "/" + outputFileName + ".dot", "UTF-8");
+            writer.println("digraph \"" + outputFileName + "\" {");
 
-			//Instantiate PrintWriter object to create and write to file. Set encoding to UTF-8
-			PrintWriter writer = new PrintWriter(dir + "/" + outputFileName + ".dot", "UTF-8");
-			writer.println("digraph \"" + outputFileName + "\" {");
+            //iterate through list and print
+            for (int i = 0; i < scheduleNodes.size(); i++) {
+                Node node = scheduleNodes.get(i);
+                writer.println("\t\t" + node.getName() + "\t\t [Weight=" + node.getWeight() + ", Start=" + node.getStartTime() + ", Processor=" + node.getProcessor() + "];");
+            }
 
-			//iterate through list and print
-			for(int i = 0; i < scheduleNodes.size(); i++){
-				Node node = scheduleNodes.get(i);
-				writer.println("\t\t" + node.getName() + "\t\t [Weight=" + node.getWeight() + ", Start=" + node.getStartTime() + ", Processor=" + node.getProcessor() + "];");
-			}
+            for (Edge edge : scheduleEdges) {
+                writer.println("\t\t" + edge.getStartNode().getName() + " -> " + edge.getEndNode().getName() + "\t [Weight=" + edge.getWeight() + "];");
+            }
 
-			for (Edge edge : scheduleEdges) {
-				writer.println("\t\t" + edge.getStartNode().getName() + " -> " + edge.getEndNode().getName() + "\t [Weight=" + edge.getWeight() + "];");
-			}
+            writer.print("}");
 
-			writer.print("}");
+            writer.close();
 
-			writer.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-
-	}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 }
