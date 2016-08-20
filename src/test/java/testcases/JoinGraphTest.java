0df453aa56053c6b4a6f25cb4c4a94102e6c6bfd
Helen Zhao <helen4251996@yahoo.co.nz>
2016-08-20 14:33:30 +1200

    Added in one join-graph test as an example

--- src/test/java/testcases/JoinGraphTest.java
+++ src/test/java/testcases/JoinGraphTest.java
@@ -1,7 +1,36 @@
 package testcases;
 
+import main.Main;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
 /**
  * Created by helen on 20/08/16.
  */
 public class JoinGraphTest {
+    String dir;
+
+    @Before
+    public void setUp() {
+        String workingDir = System.getProperty("user.dir");
+        dir = ".";
+
+        if (workingDir.length() > 0 && workingDir.contains("src")) {
+            dir = workingDir.substring(0, workingDir.indexOf(File.separator + "src"));
+        }
+    }
+
+
+    @Test
+    public void testJoinGraph5() {
+
+        String[] args = {dir +
+                "/src/test/resources/dotfiles/input/join-graph-5.dot",
+                "4"};
+
+        Main.main(args);
+
+    }
 }
