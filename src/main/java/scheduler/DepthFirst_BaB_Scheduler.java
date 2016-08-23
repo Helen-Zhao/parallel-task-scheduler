daf9cd8ab731d00bdf5035f41ac13b37868cba3b
Jay Gradon <jgra718@UoA.auckland.ac.nz>
2016-08-23 14:29:51 +1200

    Added Previously omitted change

--- src/main/java/scheduler/DepthFirst_BaB_Scheduler.java
+++ src/main/java/scheduler/DepthFirst_BaB_Scheduler.java
@@ -86,60 +86,7 @@ public class DepthFirst_BaB_Scheduler implements SchedulerInterface {
 		
 		// While not all paths have been searched (not all paths from level 0 have been searched)
 		while (level > -1) {
-			// While a complete path has not been found (not all nodes allocated)
-			while (schedule.size() < nodeList.size()) {
-				// If a node is available at this index, get it for allocation
-				if (nodeStack.get(level).size() > 0) {
-					// TODO Visualize: Nodes could be visualized on appraisal
-					node = nodeStack.get(level).peek();
-				// If a node is not available, all paths from the last scheduled node have been searched
-				} else {
-					// Return to previous level
-					returnToPreviousLevel();
-					
-					if (level < 0) {
-						// Just finished all paths, break loop
-						break;
-					}
-					// Find next node on previous level
-					continue;
-				}
-				
-				// TODO Visualize: Processors could be visualized on selection
-				// Try to allocate a processor to the node
-				// If returns false, no processors available to allocate
-				if (!processorAllocator.allocateProcessor(nodeList, node, node.getCheckedProcessors())) {
-					// Reset checked processors for this node
-					node.resetCheckedProcessors();
-					// Increment index to next node (all paths from this node have been searched)
-					nodeStack.get(level).remove();
-					// This node was not valid, find next node on this level
-					continue;
-				}
-				
-				// Add newly allocated processor to list of processors already attempted
-				node.addCheckedProcessor(node.getProcessor());
-				
-				// TODO Visualize: Newly selected node could be visualized here
-				schedule.add(node);
-				
-				// Check end time of new node against current bound
-				int nBound = node.getStartTime() + node.getWeight();
-				// If end time of new node is greater than current bound, it is the new bound
-				if (nBound > currentBound) {
-					// Check new bound does not exceed best bound; if it does, it will never be better than best
-					if (nBound > bestBound) {
-						removeLastNodeFromSchedule();
-						continue;
-					} else {
-						currentBound = nBound;
-					}
-				}
-				
-				level++;
-				nodeStack.set(level, new LinkedList<Node>(nodeFinder.findSatisfiedNodes(nodeList)));
-			}
-			
+						
 			findNewPath();
 			
 			// TODO Parallelization: BestBound should be hooked into MasterScheduler; 
@@ -254,8 +201,6 @@ public class DepthFirst_BaB_Scheduler implements SchedulerInterface {
 			Node lastNode = schedule.remove(schedule.size() - 1);
 			// Node has no longer been allocated
 			lastNode.setHasRun(false);
-			// Remove the node from the schedule
-			schedule.remove(schedule.size() - 1);
 			
 			updateHeurisitic(lastNode, false);
 		}
